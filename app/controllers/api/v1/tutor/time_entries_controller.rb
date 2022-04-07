bae16faa46b6390d6986f3f582d54b9994191a43
adilhansatemirov <adilhansatemirovv@gmail.com>
2022-04-08 00:12:51 +0600

    fix

--- app/controllers/api/v1/tutor/time_entries_controller.rb
+++ app/controllers/api/v1/tutor/time_entries_controller.rb
@@ -1,21 +1,21 @@
-class Api::V1::Freelancer::TimeEntriesController < ApplicationController
+class Api::V1::Tutor::TimeEntriesController < ApplicationController
   before_action :authenticate_api_user!, :require_freelancer
 
   # POST /time_entries
   def create
-    Freelancer::TimeEntriesService.create_time_entry(time_entry_params)
+    Tutor::TimeEntriesService.create_time_entry(time_entry_params)
     render json: { message: "time entry created" }, status: 200
   end
 
   # PUT /time_entries/:id
   def update
-    Freelancer::TimeEntriesService.update_time_entry(time_entry_params, params[:id])
+    Tutor::TimeEntriesService.update_time_entry(time_entry_params, params[:id])
     render json: { message: "time entry updated" }, status: 200
   end
 
   # DELETE /time_entries/:id
   def destroy
-    Freelancer::TimeEntriesService.delete_time_entry(params[:id])
+    Tutor::TimeEntriesService.delete_time_entry(params[:id])
     render json: { message: "time entry deleted" }, status: 200
   end 
 
